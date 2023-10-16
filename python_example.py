b5888383d72001a61d8563d02bf413a1f3b2891e
Michael Walshe <michael.j.t.walshe@gmail.com>
2023-10-16 13:09:50 +0100

    update

--- python_example.py
+++ python_example.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
 def trimmed_mean(arr: pd.Series, percent: float = 0.05):
-	arr = arr.sort_values()
-	n = arr.size
-	k = int((n * percent) / 2)
-	return arr[k:n-k].mean()
+    arr = arr.sort_values()
+    n = arr.size
+    k = int((n * percent) / 2)
+    return arr[k : n - k].mean()
