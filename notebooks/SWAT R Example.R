00b24f165b803284eb67ebfa69b64794a54a02a3
Michael Walshe <michael.j.t.walshe@gmail.com>
2024-07-29 12:57:39 +0100

    Update connection info

--- notebooks/SWAT R Example.R
+++ notebooks/SWAT R Example.R
@@ -2,13 +2,13 @@
 library(swat)
 library(ggplot2)
 
-sas_viya <-  r"(C:\SAS Viya\)"
+Sys.setenv(CAS_CLIENT_SSL_CA_LIST = "C:/SAS Viya/demo_CAS_ex_cert.crt")
 
-Sys.setenv(CAS_CLIENT_SSL_CA_LIST = file.path(sas_viya, "CAS_ex_cert.crt"))
-
-conn <- CAS(hostname="sas-cas-server-default-bin-demo.uksouth.cloudapp.azure.com",
-            port=5570,
-            authinfo=file.path(sas_viya, "authinfo"))
+conn <- CAS(
+  hostname = "sas-cas-server-default-bin-demo.uksouth.cloudapp.azure.com",
+  port = 5570,
+  authinfo = "C:/SAS Viya/.authinfo"
+)
 
 cas.builtins.serverStatus(conn)
 
@@ -16,7 +16,7 @@ cas.builtins.serverStatus(conn)
 # Create reference to data ----
 
 # Create a reference to in-memory tables, or load in local tables
-cirrhosis <- defCasTable(conn, "cirrhosis", caslib="public")
+cirrhosis <- defCasTable(conn, "cirrhosis", caslib = "public")
 
 
 
@@ -30,7 +30,7 @@ head(cirrhosis)
 
 
 # Use SAS analytics from R ----
-cas.builtins.loadActionSet(conn, actionSet="regression")
+cas.builtins.loadActionSet(conn, actionSet = "regression")
 
 output <- cas.regression.glm(
   cirrhosis,
@@ -38,7 +38,11 @@ output <- cas.regression.glm(
   inputs = c("Stage", "Status", "Drug", "Bilirubin_high", "Albumin", "Copper"),
   nominals = c("Stage"),
   output = list(
-    casOut = list(name = "PlateletsPrediction", caslib="public", replace = TRUE),
+    casOut = list(
+      name = "PlateletsPrediction",
+      caslib = "public",
+      replace = TRUE
+    ),
     copyvars = "all",
     pred = "PredictedPlatelets",
     resid = "ResidualPlatelets"
@@ -46,7 +50,7 @@ output <- cas.regression.glm(
 )
 
 
-result <- defCasTable(conn, "PlateletsPrediction", caslib="public")
+result <- defCasTable(conn, "PlateletsPrediction", caslib = "public")
 
 head(result)
 
@@ -55,14 +59,14 @@ head(result)
 plot(
   result$PredictedPlatelets,
   result$Platelets,
-  xlab="Predicted Platelets",
-  ylab="Actual Platelets"
+  xlab = "Predicted Platelets",
+  ylab = "Actual Platelets"
 )
 
 
 result |>
   to.r.data.frame() |>
-  ggplot(aes(x=PredictedPlatelets, y=Platelets)) +
+  ggplot(aes(x = PredictedPlatelets, y = Platelets)) +
   geom_point() +
-  geom_abline(intercept=0, slope=1, color="red") +
-  labs(x="Predicted Platelets", y="Actual Platelets")
+  geom_abline(intercept = 0, slope = 1, color = "red") +
+  labs(x = "Predicted Platelets", y = "Actual Platelets")
